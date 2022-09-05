30979b80cde028b03933acae52032a0e28766ce7
Simon Ericsson <simon@krlsg.se>
2022-09-05 10:56:27 +0200

    Initial commit. GET data/

---
+++ config/wexteras_example.php
@@ -0,0 +1,10 @@
+<?php
+// vim: sw=2 ts=2 et
+return array(
+  "host" => "database-host.rds.amazonaws.com",
+  "dbname" => "postgres",
+  "user" => "postgres",
+  "password" => "p455w0rd",
+  "origin" => "*",
+);
+?>
